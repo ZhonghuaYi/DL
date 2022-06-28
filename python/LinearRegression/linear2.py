a40d684d56b88296770a35f43af8e6cb5206c2b8
ZhonghuaYi <yizhonghua6@gmail.com>
2022-06-28 20:14:03 +0800

    feat:8:13PM, 6/28/2022.

---
+++ python/LinearRegression/linear2.py
@@ -0,0 +1,107 @@
+# -*- coding: utf-8 -*- 
+# @Author:  ZhonghuaYi
+# @Date  :  6/28/2022
+# @Time  :  3:58 PM
+
+from python.template import *
+
+
+class LinearData(DataSet):
+
+    def data_scale(self, x_parameter=None, y_parameter=None):
+        """将数据以其平均值和最大值归一化至(-1,1)"""
+        if x_parameter is not None and y_parameter is not None:
+            x_mean, x_maximum = x_parameter
+            y_mean, y_maximum = y_parameter
+            self.X = (self.X - x_mean) / x_maximum
+            self.Y = (self.Y - y_mean) / y_maximum
+            return (x_mean, x_maximum), (y_mean, y_maximum)
+
+        elif x_parameter is None and y_parameter is None:
+            x_mean = np.mean(self.X, axis=0)
+            x_maximum = np.max(self.X, axis=0)
+            self.X = (self.X - x_mean) / x_maximum
+            y_mean = np.mean(self.Y, axis=0)
+            y_maximum = np.max(self.Y, axis=0)
+            self.Y = (self.Y - y_mean) / y_maximum
+            return (x_mean, x_maximum), (y_mean, y_maximum)
+
+
+class SquareLoss(Loss):
+    @staticmethod
+    def loss(Y_hat, Y):
+        m = len(Y)
+        return np.sum((Y_hat - Y) ** 2. / (2. * m))
+
+
+class SquareLossDecent(GradDecent):
+    @staticmethod
+    def decent(model, lr, train_X, train_Y, train_Y_hat):
+        m = len(train_Y_hat)
+        w_grad = np.sum((train_Y_hat - train_Y) * train_X, axis=0) / m
+        b_grad = np.sum((train_Y_hat - train_Y), axis=0) / m
+        model.w_hat = model.w_hat - lr * w_grad
+        model.b_hat = model.b_hat - lr * b_grad
+
+
+class LinearTrain(Train):
+    pass
+
+
+class LinearModel(Model):
+    def __init__(self, true_w, true_b):
+        self.train_set = LinearData()
+        self.valid_set = LinearData()
+        self.test_set = LinearData()
+        self.w = np.asarray(true_w)
+        self.b = np.asarray(true_b)
+        self.w_hat = np.random.normal(0, 0.1, self.w.shape)
+        self.b_hat = np.random.normal(0, 0.1, self.b.shape)
+
+    def __str__(self):
+        return f"w:{self.w} b:{self.b}\n" \
+               f"w_hat{self.w_hat} b_hat{self.b_hat}\n"
+
+    def data_generate(self, num):
+        """产生数据"""
+        X = np.random.normal(2, 1.4, (num, len(self.w)))
+        Y = np.matmul(X, self.w) + self.b
+        Y += np.random.normal(0, 0.5, Y.shape)
+        Y = Y.reshape((-1, 1))
+        train = (X[0:int(num * 0.6), ...], Y[0:int(num * 0.6), :])
+        valid = (X[int(num * 0.6):int(num * 0.8), ...], Y[int(num * 0.6):int(num * 0.8), :])
+        test = (X[int(num * 0.8):num, ...], Y[int(num * 0.8):num, :])
+        self.train_set = LinearData(train[0], train[1])
+        self.valid_set = LinearData(valid[0], valid[1])
+        self.test_set = LinearData(test[0], test[1])
+
+    def data_scale(self):
+        """将数据以训练集的平均值和最大值归一化"""
+        x_parameter, y_parameter = self.train_set.data_scale()
+        for data in (self.valid_set, self.test_set):
+            data.data_scale(x_parameter, y_parameter)
+        w = self.w
+        b = self.b
+        self.w = x_parameter[1] * w.T / y_parameter[1]
+        self.b = (np.sum(x_parameter[0] * w.T) + b - y_parameter[0]) / y_parameter[1]
+
+    def data_plot(self, flag="2d"):
+        super().data_plot(flag)
+
+    def predict(self, label="train_set", X=None):
+        """根据X预测Y"""
+        if X is None:
+            data = self.__dict__[label]
+            Y_hat = np.matmul(data.X, self.w_hat) + self.b_hat
+        else:
+            Y_hat = np.matmul(X, self.w_hat) + self.b_hat
+        return Y_hat.reshape((-1, 1))
+
+
+if __name__ == '__main__':
+    linear_model = LinearModel([2.1, 1.4], 0.5)
+    linear_model.data_generate(1000)
+    linear_model.data_scale()
+    linear_model.train(0.1, 1000, SquareLoss.loss, SquareLossDecent.decent, LinearTrain.batch_train)
+    linear_model.data_plot(flag="3d")
+    plt.show()
--- python/template.py
+++ python/template.py
@@ -1,16 +1,17 @@
 # -*- coding: utf-8 -*- 
 # @Author:  ZhonghuaYi
-# @Time  :  2022-06-26
+# @Date  :  2022-06-28
+# @Time  :  2:56 PM
 
 import numpy as np
 import random
 from matplotlib import pyplot as plt
 
-from ..LinearRegression.linear import *
+import abc
 
 
-class DataSet:
-    """逻辑回归的数据"""
+class DataSet(metaclass=abc.ABCMeta):
+    """线性回归的数据"""
 
     def __init__(self, X=None, Y=None):
         self.X = X
@@ -33,58 +34,42 @@ class DataSet:
             ax = plt.axes(projection="3d")
             ax.scatter3D(self.X[:, 0], self.X[:, 1], self.Y, s=10, c='r')
 
-    def data_scale(self, x_parameter=None, y_parameter=None):
+    @abc.abstractmethod
+    def data_scale(self):
         """将数据以其平均值和最大值归一化至(-1,1)"""
-        if x_parameter is not None and y_parameter is not None:
-            x_mean, x_maximum = x_parameter
-            y_mean, y_maximum = y_parameter
-            self.X = (self.X - x_mean) / x_maximum
-            self.Y = (self.Y - y_mean) / y_maximum
-            return (x_mean, x_maximum), (y_mean, y_maximum)
-
-        elif x_parameter is None and y_parameter is None:
-            x_mean = np.mean(self.X, axis=0)
-            x_maximum = np.max(self.X, axis=0)
-            self.X = (self.X - x_mean) / x_maximum
-            y_mean = np.mean(self.Y, axis=0)
-            y_maximum = np.max(self.Y, axis=0)
-            self.Y = (self.Y - y_mean) / y_maximum
-            return (x_mean, x_maximum), (y_mean, y_maximum)
+        pass
 
 
-class Loss:
+class Loss(metaclass=abc.ABCMeta):
     @staticmethod
-    def cross_entropy_loss(Y_hat, Y):
-        m = len(Y)
-        return - (Y*np.log(Y_hat)+(1-Y)*np.log(1-Y_hat)) / m
+    @abc.abstractmethod
+    def loss(Y_hat, Y):
+        pass
 
 
-class GradDecent:
+class GradDecent(metaclass=abc.ABCMeta):
     @staticmethod
-    def cross_entropy_loss_decent(model, lr, X, Y, Y_hat):
-        m = len(Y_hat)
-        w_grad = - np.sum(X*(Y*(1-Y_hat)-Y_hat*(1-Y)), axis=0) / m
-        b_grad = - np.sum(Y*(1-Y_hat)-Y_hat*(1-Y), axis=0) / m
-        model.w_hat = model.w_hat - lr * w_grad
-        model.b_hat = model.b_hat - lr * b_grad
+    @abc.abstractmethod
+    def decent(model, lr, train_X, train_Y, train_Y_hat):
+        pass
 
 
 class Train:
     @staticmethod
-    def batch_train(model, loss_func, lr, epochs):
+    def batch_train(model, loss_func, decent, lr, epochs):
         losses = []
         for epoch in range(epochs):
             train_X = model.train_set.X
             train_Y = model.train_set.Y
             train_Y_hat = model.predict("train_set")
-            loss = np.sum(loss_func(train_Y_hat, model.train_set.Y))
+            loss = loss_func(train_Y_hat, model.train_set.Y)
             losses.append(loss)
             # print(f"loss before epoch{epoch}: {loss}")
-            GradDecent.cross_entropy_loss_decent(model, lr, train_X, train_Y, train_Y_hat)
+            decent(model, lr, train_X, train_Y, train_Y_hat)
         return losses
 
     @staticmethod
-    def sgd(model, loss_func, lr, epochs):
+    def sgd(model, loss_func, decent, lr):
         losses = []
         indies = list(range(len(model.train_set.Y)))
         random.shuffle(indies)
@@ -92,39 +77,34 @@ class Train:
             train_X = model.train_set.X[i, ...]
             train_Y = model.train_set.Y[i]
             train_Y_hat = model.predict(X=train_X)
-            loss = np.sum(loss_func(train_Y_hat, model.train_set.Y[i]))
+            loss = loss_func(train_Y_hat, model.train_set.Y[i])
             if i % 10 == 0:
                 losses.append(loss)
                 # print(f"loss before epoch{epoch}: {loss}")
-            GradDecent.cross_entropy_loss_decent(model, lr, train_X, train_Y, train_Y_hat)
+            decent(model, lr, train_X, train_Y, train_Y_hat)
         return losses
 
 
-class Model:
+class Model(metaclass=abc.ABCMeta):
+    @abc.abstractmethod
     def __init__(self):
         self.train_set = DataSet()
         self.valid_set = DataSet()
         self.test_set = DataSet()
-        self.w_hat = np.random.normal(0, 0.1, (1, 1))
-        self.b_hat = np.random.normal(0, 0.1, (1, 1))
 
-    def data_generate(self, num1, num2):
-        num = num1 + num2
-        X1 = np.random.normal(-2, 0.7, (num1, 1))
-        Y1 = np.zeros((num1, 1))
-        X2 = np.random.normal(2, 0.7, (num2, 1))
-        Y2 = np.ones((num2, 1))
-        Z = np.hstack((np.vstack((X1, X2)), np.vstack((Y1, Y2))))
-        np.random.shuffle(Z)
-        X = Z[:, 0:-1]
-        Y = Z[:, -1].reshape(-1, 1)
-        print(X.shape, Y.shape)
-        train = (X[0:int(num * 0.6), ...], Y[0:int(num * 0.6), :])
-        valid = (X[int(num * 0.6):int(num * 0.8), ...], Y[int(num * 0.6):int(num * 0.8), :])
-        test = (X[int(num * 0.8):num, ...], Y[int(num * 0.8):num, :])
-        self.train_set = DataSet(train[0], train[1])
-        self.valid_set = DataSet(valid[0], valid[1])
-        self.test_set = DataSet(test[0], test[1])
+    @abc.abstractmethod
+    def __str__(self):
+        pass
+
+    @abc.abstractmethod
+    def data_generate(self, *args):
+        """产生数据"""
+        pass
+
+    @abc.abstractmethod
+    def data_scale(self):
+        """将数据以训练集的平均值和最大值归一化"""
+        pass
 
     def data_plot(self, flag="2d"):
         """绘制训练集、验证集、测试集在一个图中"""
@@ -150,9 +130,30 @@ class Model:
                          s=10,
                          c='y')
 
+    @abc.abstractmethod
+    def predict(self, X):
+        """根据X预测Y"""
+        pass
 
-if __name__ == '__main__':
-    logistic = Model()
-    logistic.data_generate(500, 500)
-    logistic.data_plot()
-    plt.show()
+    def train(self, lr, epochs, loss_func, decent, training):
+        # 最初的参数
+        print(f"Initial value:\n{self}\n")
+        # 开始训练，获得losses
+        losses = training(self, loss_func, decent, lr, epochs)
+        # 训练集最终loss
+        loss = loss_func(self.predict("train_set"), self.train_set.Y)
+        print(f"End Loss: {loss}")
+        # 梯度下降后得到的结果
+        print(f"Result:\n{self}\n")
+        # 验证集loss
+        valid_loss = loss_func(self.predict("valid_set"), self.valid_set.Y)
+        print(f"Validation Set Loss: {valid_loss}")
+        # 测试集loss
+        test_loss = loss_func(self.predict("test_set"), self.test_set.Y)
+        print(f"Test Set Loss: {test_loss}")
+        # loss曲线
+        plt.plot(np.array(list(range(len(losses)))), np.array(losses))
+        plt.title("Loss-Epoch relation")
+        plt.xlabel("epoch")
+        plt.ylabel("loss")
+        plt.show()
